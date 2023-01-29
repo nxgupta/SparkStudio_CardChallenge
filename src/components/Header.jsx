9d895f4c7cf28514692ca7c962c6bc5ba60e1f8d
nxgupta <neeraj.gupta6525@gmail.com>
2023-01-29 23:59:31 +0530

    final submission

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Box, Button, Stack } from "@mui/material"
 import data from "../utils/data"
-import { useTheme } from "@emotion/react"
+import styled from "@emotion/styled"
 
 const Header = ({ category, minAge, maxAge, setState, show }) => {
   const handleShow = (category) => {
@@ -17,55 +17,43 @@ const Header = ({ category, minAge, maxAge, setState, show }) => {
       })
     }
   }
-
-  let theme = useTheme();
   let { category_name } = data[0];
 
-  return (
-    <Box sx={{
+  let StyledBox = styled(Box)(({ theme }) => ({
+    backgroundColor: show ? (theme.palette[category_name].dark) : (theme.palette[category_name].light),
+    position: 'relative',
+    height: '100px',
+    width: '147px',
+    borderRadius: '4px',
+    color: show ? '#FFFFFF' : '#333333',
+    cursor: 'pointer',
+    margin: "20px 0 30px 0",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    '& .triangle': {
       backgroundColor: show ? theme.palette[category_name].dark : theme.palette[category_name].light,
-      position: 'relative',
-      height: '100px',
-      width: '147px',
-      borderRadius: '4px',
-      color: show?'#FFFFFF':'#333333',
-      cursor: 'pointer',
-      margin: "20px 0 30px 0",
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-      onClick={() => handleShow(category)}>
-      <Stack sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
+      width: "20px",
+      height: "20px",
+      transform: "rotate(45deg)",
+      position: "absolute",
+      bottom: "-10px",
+      zIndex: 5
+    }
+  }))
+
+
+  return (
+    <StyledBox onClick={() => handleShow(category)}>
+      <Stack sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <Box sx={{
-          fontWeight: '700',
-          fontSize: '24px',
-          lineHeight: '36px',
-          fontFamily: '"Poppins" !important'
+          fontWeight: '700', fontSize: '24px', lineHeight: '36px', fontFamily: '"Poppins" !important'
         }}>{category}</Box>
-        <Box sx={{
-          fontWeight: '500',
-          fontSize: '18px',
-          lineHeight: '27px',
-          fontFamily: '"Poppins" !important'
-        }}>(Age {minAge}-{maxAge})</Box>
+        <Box sx={{ fontWeight: '500', fontSize: '18px', lineHeight: '27px', fontFamily: '"Poppins" !important' }}>(Age {minAge}-{maxAge})</Box>
       </Stack>
-      {show ? <Box
-        sx={{
-          backgroundColor: show ? theme.palette[category_name].dark : theme.palette[category_name].light,
-          width: "20px",
-          height: "20px",
-          transform: "rotate(45deg)",
-          position: "absolute",
-          bottom: "-10px",
-          zIndex: 5
-        }}
-      ></Box> : null}
-    </Box>
+      {show ? <Box className='triangle'></Box> : null}
+    </StyledBox>
   )
 }
 export default Header
